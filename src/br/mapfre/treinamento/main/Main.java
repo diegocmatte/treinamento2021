e024c1d9f7116f65d1e2a8c96e052d1f28ec7480
Diego Matté <diegocmatte@gmail.com>
2021-11-04 14:46:58 -0300

    treinamento 2021 java

--- src/br/mapfre/treinamento/main/Main.java
+++ src/br/mapfre/treinamento/main/Main.java
@@ -6,13 +6,14 @@ import br.mapfre.treinamento.model.Vendedor;
 public class Main{
 
     public static void main(String[] args) {
-        Vendedor vendedor = new Vendedor("abc", "123", "mat123", 1500.00, 0.25);
+        Vendedor vendedor = new Vendedor("vendedor", "123", "mat123", 1500.00, 0.25);
         System.out.println("Salario vendedor: " + vendedor.getSalario());
         System.out.println("Bonificacao vendedor: " +vendedor.getBonificacao());
         System.out.println("---------------------");
 
-        Gerente gerente = new Gerente("abc", "123", "mat123", 1500.00, 0.25, 1, "T.I.");
+        Gerente gerente = new Gerente("gerente", "123", "mat123", 1500.00, 0.25, 1, "T.I.");
         System.out.println("Salario gerente: " + gerente.getSalario());
         System.out.println("Salario gerente: " + gerente.getBonificacao());
+        System.out.println("---------------------");
     }
 }
